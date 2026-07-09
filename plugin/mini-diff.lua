dee6347cfe7505ef50a5d5de7bf25f0fb363ede3
Richard Baptist <code@richardbaptist.nl>
2026-07-09 12:34:10 +0200

    Fix diff icons

--- plugin/mini-diff.lua
+++ plugin/mini-diff.lua
@@ -2,7 +2,16 @@ vim.pack.add({
   { src = "https://github.com/nvim-mini/mini.diff" },
 }, { confirm = false })
 
-require("mini.diff").setup({})
+require("mini.diff").setup({
+  view = {
+    style = "sign",
+    signs = {
+      add = "+",
+      change = "▪",
+      delete = "",
+    },
+  },
+})
 
 vim.keymap.set("n", "<leader>gd", function()
   require("mini.diff").toggle_overlay()
