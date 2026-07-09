2fbf61b579d961a030f2d46789f97df8eb2d1377
Richard Baptist <code@richardbaptist.nl>
2026-07-09 20:15:42 +0200

    Fix keymap

--- plugin/mini-move.lua
+++ plugin/mini-move.lua
@@ -4,13 +4,13 @@ vim.pack.add({
 
 require("mini.move").setup({
   mappings = {
-    left = "<A-Left>",
-    right = "<A-Right>",
-    down = "<A-Down>",
-    up = "<A-Up>",
-    line_left = "<A-Left>",
-    line_right = "<A-Right>",
-    line_down = "<A-Down>",
-    line_up = "<A-Up>",
+    left = "<C-S-Left>",
+    right = "<C-S-Right>",
+    down = "<C-S-Down>",
+    up = "<C-S-Up>",
+    line_left = "<C-S-Left>",
+    line_right = "<C-S-Right>",
+    line_down = "<C-S-Down>",
+    line_up = "<C-S-Up>",
   },
 })
