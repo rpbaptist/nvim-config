4f2f8c6ecbc121ef9130271783f1a9c9ab3cd66b
Richard Baptist <code@richardbaptist.nl>
2026-07-13 12:59:00 +0200

    Fix luasnip config

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -1,8 +1,7 @@
 vim.pack.add({
   { src = "https://github.com/L3MON4D3/LuaSnip" },
-  { src = "https://github.com/rafamadriz/friendly-snippets", config = function()
-    require("luasnip.loaders.from_vscode").lazy_load()
-  end },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
 }, { confirm = false })
 
 require("luasnip").setup({})
+require("luasnip.loaders.from_vscode").lazy_load()
